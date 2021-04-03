e68fbba10575f566f6616a8c86e83c44e138517c
Nikrajput <nikrajput8383@gmail.com>
2021-04-03 12:51:43 +0530

    file upload complete

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,9 @@
 const express=require('express')
 const router=express.Router()
-const multer=require('multer')
-const path=require('path')
-const fs=require('fs')
 const Book=require('../models/book')
 const Author=require('../models/author')
 
-const uploadPath=path.join('public',Book.coverImageBasePath)
-
-const imagesMimeTypes=['image/jpeg','image/png','image/gif']
-
-const upload=multer({
-    dest:uploadPath,
-    fileFilter:(req,file,callback)=>{
-        callback(null,imagesMimeTypes.includes(file.mimetype))
-    }
-})
+const imageMimeTypes=['image/jpeg','image/png','image/gif']
 
 router.get('/',async(req,res)=>{
 
@@ -46,18 +34,17 @@ router.get('/new',async(req,res)=>{
     renderNewPage(res,new Book(),false)
 })
 
-router.post('/',upload.single('cover'),async(req,res)=>{
-
-    const fileName=req.file!=null ? req.file.filename : null
+router.post('/',async(req,res)=>{
 
     const book=new Book({
         title:req.body.title,
         author:req.body.author,
         publishDate:new Date(req.body.publishDate),
         pageCount:req.body.pageCount,
-        coverImageName:fileName,
         description:req.body.description
     })
+
+    saveCover(book,req.body.cover)
 
     try{
         const newBook=await book.save()
@@ -65,17 +52,17 @@ router.post('/',upload.single('cover'),async(req,res)=>{
         res.redirect('books')
     }
     catch{
-        if(book.coverImageName!=null){
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res,book,true)
     }
 })
 
-function removeBookCover(fileName){
-    fs.unlink(path.join(uploadPath,fileName),(err)=>{
-        if(err) console.error(err)
-    })
+function saveCover(book,coverEncoded){
+    if(coverEncoded==null)return
+    const cover=JSON.parse(coverEncoded)
+    if(cover!=null && imageMimeTypes.includes(cover.type)){
+        book.coverImage=new Buffer.from(cover.data,'base64')
+        book.coverImageType=cover.type
+    }
 }
 
 async function renderNewPage(res,book,hasError){
